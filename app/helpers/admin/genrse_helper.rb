e4cf73661d3c676ef83737cd98113dc27e8b499e
EC2 Default User <ec2-user@ip-172-31-42-218.ec2.internal>
2022-07-05 09:31:42 +0000

    initial commit

---
+++ app/helpers/admin/genrse_helper.rb
@@ -0,0 +1,2 @@
+module Admin::GenrseHelper
+end
---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :genre
+  
+  def add_tax_price
+    (self.price*1.10).round
+  end
+  
+  
+  def get_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/image.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width,height]).processed
+  end
+end
